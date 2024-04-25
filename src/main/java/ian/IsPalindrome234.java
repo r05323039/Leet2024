076a99e54d9823145f3e1888d4bc0f0247c09c7f
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2024-04-25 19:55:04 +0800

    no message

---
+++ src/main/java/ian/IsPalindrome234.java
@@ -0,0 +1,18 @@
+package ian;
+
+import org.junit.jupiter.api.Assertions;
+
+public class IsPalindrome234 extends ListNode {
+    public IsPalindrome234(int val, ListNode next) {
+        super(val, next);
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/ian/MiddleNode876.java
+++ src/main/java/ian/MiddleNode876.java
@@ -1,6 +1,7 @@
 package ian;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MiddleNode876 extends ListNode {
     public MiddleNode876(int val, ListNode next) {
@@ -15,13 +16,25 @@ public class MiddleNode876 extends ListNode {
         return head;
     }
 
+    public static ListNode middleNode2(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
+    }
+
     public static void main(String[] args) {
         ListNode n = getNodes(1, 2, 3, 4, 5);
         System.out.println(n);
         Assertions.assertEquals(3, middleNode(n).val);
+        Assertions.assertEquals(3, middleNode2(n).val);
 
         ListNode n2 = getNodes(1, 2, 3, 4, 5, 6);
         System.out.println(n2);
         Assertions.assertEquals(4, middleNode(n2).val);
+        Assertions.assertEquals(4, middleNode2(n2).val);
     }
 }
