3c1d88fb0ccccb39f66e6fbf08256fb107c8805d
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2024-04-26 12:33:46 +0800

    no message

--- src/main/java/ian/IsPalindrome234.java
+++ src/main/java/ian/IsPalindrome234.java
@@ -7,12 +7,37 @@ public class IsPalindrome234 extends ListNode {
         super(val, next);
     }
 
-    public boolean isPalindrome(ListNode head) {
-        return false;
+    public static boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+
+        int i = 0, j = sb.length() - 1;
+        while (i <= j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 
 
     public static void main(String[] args) {
+        ListNode n1 = getNodes(1, 2, 2, 1);
+        System.out.println(n1);
+        Assertions.assertTrue(isPalindrome(n1));
 
+        ListNode n2 = getNodes(1, 2, 3, 2, 1);
+        System.out.println(n2);
+        Assertions.assertTrue(isPalindrome(n2));
+
+        ListNode n3 = getNodes(1, 2, 3, 1);
+        System.out.println(n3);
+        Assertions.assertFalse(isPalindrome(n3));
     }
 }
